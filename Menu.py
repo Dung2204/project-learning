0772ec9f64c419b43080d781ce08b6d7edba99ea
Phan Tan Dung <phantandung2003@gmail.com>
2025-04-27 22:27:07 +0700

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -4,18 +4,18 @@ import streamlit as st
 from DeleteImg import run_DeleteBackground_app
 from Language import run_Language_Check_app
 
-# Cấu hình trang chính - phải được gọi ngay đầu file
+# Cấu hình trang chính
 st.set_page_config(page_title="Multi-App", layout="wide")
 
 # Sidebar chứa menu ứng dụng
 st.sidebar.title("Home page")
 app_choice = st.sidebar.selectbox(
     "Chọn ứng dụng:",
-    ["DeleteBackgroundImg", "Language Check"]
+    ["Delete Background Image", "Language Check"]
 )
 
 # Điều hướng đến ứng dụng được chọn
 if app_choice == "Delete Background Image":
     run_DeleteBackground_app()
-elif app_choice =="Language Check":
+elif app_choice == "Language Check":
     run_Language_Check_app()
